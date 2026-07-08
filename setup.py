6d62fd14bcdd227bf1c7614ded408f2b6f7d4a4a
SiNeFy <nathan33324@gmail.com>
2026-07-08 10:57:12 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'zed_lidar_fusion'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='chan_baby',
+    maintainer_email='nathan33324@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'fusion_node = zed_lidar_fusion.fusion_node:main'
+        ],
+    },
+)
